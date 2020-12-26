cc2e3b1a3d7e0c443ada121bde80175fa46cb054
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-12-26 09:57:01 +0530

    style: SUBMISSION - 7.13 - add final styling to user stats table in users view

--- app4-bloglist/bloglist-frontend/src/components/UserStats.js
+++ app4-bloglist/bloglist-frontend/src/components/UserStats.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getUserStatsPopulateActionAsync } from '../reducers/userStatsReducer.js'
+import './css/UserStats.css'
 
 
 const StatContainer = ({ userStatsObj }) => {
