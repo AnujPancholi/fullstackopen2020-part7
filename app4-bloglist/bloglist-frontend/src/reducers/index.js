c1775cf8faca78b5ba111f305b9901d78d52a954
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-12-27 12:13:45 +0530

    fix: remove featuredUserReducer which was unnecessary

--- app4-bloglist/bloglist-frontend/src/reducers/index.js
+++ app4-bloglist/bloglist-frontend/src/reducers/index.js
@@ -3,7 +3,6 @@ import notificationReducer from './notificationReducer.js'
 import blogsReducer from './blogsReducer.js'
 import loginReducer from './loginReducer.js'
 import userStatsReducer from './userStatsReducer'
-import featuredUserReducer from './featuredUserReducer.js'
 
 
 
@@ -11,8 +10,7 @@ const reducer = combineReducers({
   notification: notificationReducer,
   blogs: blogsReducer,
   auth: loginReducer,
-  user_stats: userStatsReducer,
-  featured_user: featuredUserReducer
+  user_stats: userStatsReducer
 })
 
 export default reducer
