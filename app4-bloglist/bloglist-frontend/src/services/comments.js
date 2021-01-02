9be1e07010f16c00cef326b4a5ab63d425607e09
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2021-01-02 11:40:43 +0530

    feat: add function to add comment in commentService

--- app4-bloglist/bloglist-frontend/src/services/comments.js
+++ app4-bloglist/bloglist-frontend/src/services/comments.js
@@ -34,7 +34,45 @@ const getCommentsForBlog = (__blogId) => {
   })
 }
 
+const addComment = (__comment, __user) => {
+  return new Promise((resolve,reject) => {
+    (async() => {
+
+      try{
+        const commentAdditionAxiosResult = await commentsAxios({
+          method: 'POST',
+          url: '/',
+          headers: {
+            'Authorization': `Bearer ${__user.token}`
+          },
+          data: __comment
+        })
+
+        if(!commentAdditionAxiosResult.data || !commentAdditionAxiosResult.data.comment){
+          throw new Error('COMMENT NOT FOUND IN RESPONSE')
+        }
+
+        resolve(commentAdditionAxiosResult.data.comment)
+
+      } catch(e) {
+        if(e.response){
+          reject(e.response.data)
+        } else if(e.request){
+          reject({
+            message: 'NO RESPONSE FROM SERVER'
+          })
+        } else {
+          reject({
+            message: 'AN ERROR OCCURRED'
+          })
+        }
+      }
+    })()
+  })
+}
+
 
 export default {
-  getCommentsForBlog
+  getCommentsForBlog,
+  addComment
 }
